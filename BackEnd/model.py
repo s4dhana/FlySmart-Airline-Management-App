19495ac4859672046843c5bdfd61be1e7bb42b48
s4dhana <sadana.spk@gmail.com>
2024-10-02 21:10:07 +0530

    Create model.py

---
+++ BackEnd/model.py
@@ -0,0 +1,68 @@
+import anvil.server
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM
+
+@anvil.server.callable
+def load_and_preprocess_data():
+    df = anvil.server.request.get_file('weather_data.csv')
+    data = pd.read_csv(df, parse_dates=['Date'], index_col='Date')
+    
+    new_data = data[['Windspeed']].values
+
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled_data = scaler.fit_transform(new_data)
+
+    return scaled_data, scaler
+
+# Create the LSTM model
+def create_model():
+    model = Sequential()
+
+    # Add LSTM layers
+    model.add(LSTM(units=50, return_sequences=True, input_shape=(60, 1)))
+    model.add(LSTM(units=50, return_sequences=False))
+
+    # Add Dense layers
+    model.add(Dense(units=25))
+    model.add(Dense(units=1))
+
+    # Compile the model
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    
+    return model
+
+@anvil.server.callable
+def train_model():
+    scaled_data, scaler = load_and_preprocess_data()
+    
+    def create_dataset(data, look_back=60):
+        X, y = [], []
+        for i in range(look_back, len(data)):
+            X.append(data[i-look_back:i, 0])
+            y.append(data[i, 0])
+        return np.array(X), np.array(y)
+
+    look_back = 60
+    X, y = create_dataset(scaled_data, look_back)
+    X = np.reshape(X, (X.shape[0], X.shape[1], 1))
+
+    model = create_model()
+    model.fit(X, y, batch_size=32, epochs=5)
+
+    return model, scaler
+
+@anvil.server.callable
+def make_prediction():
+    model, scaler = train_model()
+
+    scaled_data, _ = load_and_preprocess_data()
+    test_data = scaled_data[-60:]
+    X_test = np.reshape(test_data, (1, test_data.shape[0], 1))
+
+    predicted_res = model.predict(X_test)
+    predicted_res = scaler.inverse_transform(predicted_res)
+
+    return predicted_res[0][0]
